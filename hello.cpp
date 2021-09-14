af3233369d1e3cf4d5088c5dc022e4e9ac1053f2
Mahima Khatri <66864785+Mahi8@users.noreply.github.com>
2021-09-14 21:12:01 +0530

    New change made

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"Hello World"<<endl;
-    cout<<"How are you doing";
+    cout<<"How are you doing"<<endl;
+    cout<<"Welcome to Dev";
     return 0;
 }
